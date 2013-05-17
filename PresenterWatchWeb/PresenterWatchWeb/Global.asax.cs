e2f86dfd70adcf29b5f63c07897b5febb32c915d
Valery Jacobs <valeryjacobs@hotmail.com>
2013-05-17 14:12:29 +0200

    c

--- PresenterWatchWeb/PresenterWatchWeb/Global.asax.cs
+++ PresenterWatchWeb/PresenterWatchWeb/Global.asax.cs
@@ -18,6 +18,9 @@ namespace PresenterWatchWeb
         {
             AreaRegistration.RegisterAllAreas();
 
+            RouteTable.Routes.MapHubs();
+
+
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- PresenterWatchWeb/PresenterWatchWeb/Hubs/SpeakerWatchHub.cs
+++ PresenterWatchWeb/PresenterWatchWeb/Hubs/SpeakerWatchHub.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace PresenterWatchWeb.Hubs
 {
-    public class SpeakerWatchHub
+    public class SpeakerWatchHub : Hub
     {
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
     }
 }
